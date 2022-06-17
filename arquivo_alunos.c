1dc90b283d3d6fd778f62ac54f73a76698ac3ee5
Paulo-vca <paulovca26@gmail.com>
2022-06-17 13:52:09 -0400

    subindo

--- arquivo_alunos.c
+++ arquivo_alunos.c
@@ -1,5 +1,4 @@
 /*MODELO TABELA EXIBIÇÃO:
-
 PERIODO MATERIA	ALUNO	GENERO	IDADE	N1	N2	N3	MEDIA	SITUAÇÃO*/
 
 #include <stdio.h>
@@ -17,12 +16,57 @@ void cinco();
 
 int main(){
     setlocale(LC_ALL, "portuguese-brazilian");
-    int op;
+    int op, cont = 1;
 
-    //ABRINDO E LENDO O ARQUIVO
+
+    while(cont==1){
+        printf("=====================================================\n");
+        printf("OPÇÕES\n");
+        printf("=====================================================\n");
+        printf("1 - Apresentar dados dos arquivos\n");
+        printf("2 - Exibir alunos matriculados em determinada materia\n");
+        printf("3 - Calcular média\n");
+        printf("4 - Situação do aluno\n");
+        printf("5 - Sair\n");
+        printf("=====================================================\n");
+        scanf("%d", &op);
+
+        switch(op){
+
+            case 1:
+                um();
+            break;
+
+            case 2:
+                dois();
+            break;
+
+            case 3:
+                tres();
+            break;
+
+            case 4:
+                quatro();
+            break;
+            case 5:
+                cinco();
+            break;
+            default:
+                cont++;
+                printf("Você escolheu sair...\n");
+            }
+
+    }
+
+return 0;
+}
+
+void um(){
+
+     //ABRINDO E LENDO O ARQUIVO
 
     int id;
-    char periodo[10];
+    char periodo[11];
     char materia[50];
     char aluno[30];
     char genero;
@@ -42,6 +86,71 @@ int main(){
         return 1;
     }
 
+    while (fscanf(arq, "%d ", &id) != EOF){
+        fscanf(arq, "%s ", &periodo);
+        fscanf(arq, "%s ", &materia);
+        fscanf(arq, "%s ", &aluno);
+        fscanf(arq, "%c ", &genero);
+        fscanf(arq, "%d ", &idade);
+        fscanf(arq, "%f ", &n1);
+        fscanf(arq, "%f ", &n2);
+        fscanf(arq, "%f", &n3);
+
+
+
+    //APRESENTANDO ARQUIVO
+
+        printf("ID: %d \n", id);
+        printf("Periodo: %s \n",periodo);
+        printf("Materia: %s \n",materia);
+        printf("Aluno: %s \n",aluno);
+        printf("Genero: %c \n",genero);
+        printf("Idade: %d \n",idade);
+        printf("Nota 01: %.2f \n",n1);
+        printf("NOta 02: %.2f \n",n2);
+        printf("Nota 03: %.2f \n\n",n3);
+
+        sleep(1);
+    }
+    fclose(arq);
+
+}
+void dois(){
+
+ //ABRINDO E LENDO O ARQUIVO
+
+    int id;
+    char periodo[11];
+    char materia[50];
+    char aluno[30];
+    char genero;
+    int idade;
+    float n1;
+    float n2;
+    float n3;
+    float media;
+    char situacao;
+    int op1;
+
+    FILE *arq;
+
+    arq = fopen("Alunos.txt", "r");
+
+    if (arq == NULL){
+        printf("Erro ao abrir o arquivo!");
+        return 1;
+    }
+
+
+    printf("\nVisualizar alunos em tal materia\n");
+    printf("=====================================================\n");
+    printf("OPÇÕES\n");
+    printf("=====================================================\n");
+    printf("123 - INTERFACE-HOMEM-COMPUTADOR\n");
+    printf("234 - PROGRAMAÇÃO-DE-COMPUTADORES\n");
+    printf("345 - LABORATORIO-DE-SISTEMAS-OPERACIONAIS\n");
+    printf("=====================================================\n");
+    scanf("%d", &op1);
 
     while (fscanf(arq, "%d ", &id) != EOF){
         fscanf(arq, "%s ", &periodo);
@@ -51,55 +160,32 @@ int main(){
         fscanf(arq, "%d ", &idade);
         fscanf(arq, "%f ", &n1);
         fscanf(arq, "%f ", &n2);
-        fscanf(arq, "%f ", &n3);
+        fscanf(arq, "%f", &n3);
+
+        if(id == op1){
+
+        //APRESENTANDO ARQUIVO
+
+            printf("ID: %d \n", id);
+            printf("Periodo: %s \n",periodo);
+            printf("Materia: %s \n",materia);
+            printf("Aluno: %s \n",aluno);
+            printf("Genero: %c \n",genero);
+            printf("Idade: %d \n",idade);
+            printf("Nota 01: %.2f \n",n1);
+            printf("NOta 02: %.2f \n",n2);
+            printf("Nota 03: %.2f \n\n",n3);
+
+            sleep(1);
+
+        }
     }
 
     fclose(arq);
-
-    do{
-        printf("=====================================================\n");
-        printf("OPÇÕES\n");
-        printf("=====================================================\n");
-        printf("1 - Apresentar dados dos arquivos\n");
-        printf("2 - Realizar matricula de aluno\n");
-        printf("3 - Lançar notas\n");
-        printf("4 - Calcular média\n");
-        printf("5 - Situação do aluno\n");
-        printf("6 - Sair\n");
-        printf("=====================================================\n");
-        scanf("%d", &op);
-
-        switch(op){
-
-            case 1:
-                um();
-            break;
-            
-            case 2:
-                dois();
-            break;
-
-            case 3:
-                tres();
-            break;
-
-            case 4:
-                quatro;
-            break;
-            case 5:
-                cinco;
-            break;
-            default:
-                printf("Você escolheu sair...\n");
-        }
-
-    }while(op >= 1 && op <= 5);
-
-return 0;
 }
 
-void um(){ }
-void dois(){ }
 void tres(){ }
+
 void quatro(){ }
+
 void cinco(){ }
